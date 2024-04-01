602fd845f45892df6cc1ad18b53b1469e29fdfb5
redesigned <josh@redesigned.com>
2024-04-01 18:30:28 -0400

    addition: Generate AES-CBC Encryption Key.

--- algorithms/cryptography/encryptionAES-CBC.js
+++ algorithms/cryptography/encryptionAES-CBC.js
@@ -1,3 +1,36 @@
 // @ts-check
 "use strict";
 
+/**
+ * Generate AES-CBC Encryption Key
+ * Generates an AES-CBC key asynchronously.
+ * @returns {Promise<string>} The base64-encoded key.
+ */
+export async function generateAESGCMKey() {
+	let new_key = {};
+	await window.crypto.subtle.generateKey(
+		{
+			name: 'AES-CBC',
+			length: 256,
+		},
+		true,
+		['encrypt', 'decrypt']
+	)
+	.then(async (key) => {
+		await window.crypto.subtle.exportKey(
+			'jwk',
+			key
+		)
+		.then((keydata) => {
+			new_key = keydata;
+		})
+		.catch((err) => {
+			console.error(err);
+		});
+	})
+	.catch((err) => {
+		console.error(err);
+	});
+	return await new_key.k;
+}
+
