3ce8ea49356bbf008192df1d2abfc951edd4f441
redesigned <josh@redesigned.com>
2024-04-01 18:25:55 -0400

    addition: Generate AES-GCM Encryption Key.

--- algorithms/cryptography/encryptionRSA-OAEP.js
+++ algorithms/cryptography/encryptionRSA-OAEP.js
@@ -1,3 +1,36 @@
 // @ts-check
 "use strict";
 
+/**
+ * Generate AES-GCM Encryption Key
+ * Generates an AES-GCM key asynchronously.
+ * @returns {Promise<string>} The base64-encoded key.
+ */
+export async function generateAESGCMKey() {
+	let new_key = '';
+	await window.crypto.subtle.generateKey(
+		{
+			name: 'AES-GCM',
+			length: 256,
+		},
+		true,
+		['encrypt', 'decrypt']
+	)
+	.then(async (key) => {
+		await window.crypto.subtle.exportKey(
+			'jwk',
+			key
+		)
+		.then((keydata) => {
+			new_key = keydata;
+		})
+		.catch((err) => {
+			console.error(err);
+		});
+	})
+	.catch((err) => {
+		if (window.debugging) { console.error(err); };
+	});
+	return await new_key.k;
+}
+
