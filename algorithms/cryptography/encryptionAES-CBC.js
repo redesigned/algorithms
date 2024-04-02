afff9a89d4baf1885d07ba6eb35a98db268d73b8
redesigned <josh@redesigned.com>
2024-04-01 20:09:08 -0400

    addition: Decrypt AES-CBC.

--- algorithms/cryptography/encryptionAES-CBC.js
+++ algorithms/cryptography/encryptionAES-CBC.js
@@ -74,7 +74,7 @@ export async function encryptAESCBC(key, secret, data) {
 			let binary = '';
 			const bytes = new Uint8Array(encrypted);
 			for (let i = 0; i < bytes.byteLength; i++) { binary += String.fromCharCode(bytes[i]); };
-			encoded_data = `${encodeURIComponent(window.btoa(binary))}`;
+			encoded_data = `${new Uint8Array(encrypted, 0, 5)}`;
 		})
 		.catch((err) => {
 			console.error(err);
@@ -88,3 +88,56 @@ export async function encryptAESCBC(key, secret, data) {
 	return await encoded_data;
 }
 
+/**
+ * decrypt AES-CBC
+ * Decrypts text via AES-GCM encrption asynchronously.
+ * @param {string} key - The base64-encoded encryption key.
+ * @param {string} secret - A password
+ * @param {string} data -The text to decrypt
+ * @returns {Promise<string>} - The decrypted text.
+ */
+export async function decryptAESCBC(key, secret, data) {
+	let decoded_data = '';
+	if (secret.length < 1) { secret = '$3(63+$^|+__Utw:afXjtE,viA>ji2k>.CC_'; };
+	secret = new TextEncoder().encode(secret.slice(0, 16).padStart(16, " "));
+	const binary_string = window.atob(`${decodeURIComponent(data)}`);
+	let bytes = new Uint8Array(binary_string.length);
+	for (let i = 0; i < binary_string.length; i++){ bytes[i] = binary_string.charCodeAt(i); };
+	data = bytes.buffer;
+	await window.crypto.subtle.importKey(
+		'jwk',
+		{
+			kty: 'oct',
+			k: key,
+			alg: 'A256CBC',
+			ext: true,
+		},
+		{
+			name: 'AES-CBC',
+		},
+		false,
+		['decrypt']
+	)
+	.then(async (key) => {
+		await window.crypto.subtle.decrypt(
+			{
+				name: 'AES-CBC',
+				iv: secret,
+			},
+			key,
+			data
+		)
+		.then((decrypted) => {
+			decoded_data = new TextDecoder().decode(new Uint8Array(decrypted));
+		})
+		.catch((err) => {
+			console.error(err);
+			throw err;
+		});
+	})
+	.catch((err) => {
+		console.error(err);
+		throw err;
+	});
+	return await decoded_data;
+}
