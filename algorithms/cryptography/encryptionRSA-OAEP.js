3d02f2e7afc4e12f3c4eed7bf615d6359f04135b
redesigned <josh@redesigned.com>
2024-04-01 20:42:06 -0400

    addition: Generate RSA-OAEP Encryption Key. Generate RSA-OAEP Decryption Key.

--- algorithms/cryptography/encryptionRSA-OAEP.js
+++ algorithms/cryptography/encryptionRSA-OAEP.js
@@ -6,7 +6,7 @@
  * Generates an RSA-OAEP key asynchronously.
  * @returns {Promise<string>} The base64-encoded key.
  */
-export async function generateRSAOAEPKey() {
+export async function generateRSAOAEPKeyEncryption() {
 	let new_key = {};
 	await crypto.subtle.generateKey(
 		{
@@ -36,3 +36,37 @@ export async function generateRSAOAEPKey() {
 	return await new_key.n;
 }
 
+/**
+ * Generate RSA-OAEP Decryption Key
+ * Generates an RSA-OAEP key asynchronously.
+ * @returns {Promise<string>} The base64-encoded key.
+ */
+export async function generateRSAOAEPKeyDecryption() {
+	let new_key = {};
+	await crypto.subtle.generateKey(
+		{
+			name: "RSA-OAEP",
+			modulusLength: 4096,
+			publicExponent: new Uint8Array([1, 0, 1]),
+			hash: "SHA-256",
+		},
+		true,
+		['encrypt', 'decrypt']
+	)
+	.then(async (key) => {
+		await crypto.subtle.exportKey(
+			'jwk',
+			key.privateKey
+		)
+		.then((keydata) => {
+			new_key = keydata;
+		})
+		.catch((err) => {
+			console.error(err);
+		});
+	})
+	.catch((err) => {
+		console.error(err);
+	});
+	return await new_key.n;
+}
