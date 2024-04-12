f3b82b9a8991971c60d18a82e6084fb4e8347ea6
redesigned <josh@redesigned.com>
2024-04-12 19:23:30 -0400

    started: mapped out circle math functions to code.

---
+++ algorithms/math/circle.js
@@ -0,0 +1,23 @@
+// radius to diameter
+
+// radius t0 circumferance
+
+// radius to area
+
+// diameter to radius
+
+// diameter to circumference
+
+// diameter to area
+
+// circumference to radius
+
+// circumference to diameter
+
+// area to radius
+
+// area to diameter
+
+// circumference to area
+
+// area to circumference
