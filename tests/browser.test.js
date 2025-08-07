6927a8e305bc95adc3b0fd030fe8f35028594a7c
redesigned <josh@redesigned.com>
2025-08-06 21:21:17 -0700

    add: test for isMobile

--- tests/browser.test.js
+++ tests/browser.test.js
@@ -3,6 +3,6 @@ import { expect, test } from 'vitest';
 /* ## BROWSER ## */
 
 import { isMobile } from '../algorithms/browser/isMobile';
-test('Is Mobile:', () => {
+test('Is Mobile: O(1)', () => {
 	expect(isMobile()).toBeTypeOf('boolean');
 });
